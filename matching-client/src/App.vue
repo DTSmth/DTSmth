1f5c7915b9d13e3d7d1ae656de4bcc478b6618a9
DTSmth <ducktapedave85@gmail.com>
2024-01-19 21:58:12 -0500

    client side vue project started

---
+++ matching-client/src/App.vue
@@ -0,0 +1,52 @@
+
+<template>
+  <div>
+    <header>
+      <h1> Testing dashboard</h1>
+    </header>
+    <section>
+      <ClientInfo />
+    </section>
+  </div>
+</template>
+
+<script>
+import ClientInfo from './components/ClientInfo.vue'; // Import the component
+ 
+export default {
+  name: 'App',
+  components: {
+    ClientInfo // Add the component to your component list
+  }
+}
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ matching-client/src/components/ClientInfo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <!-- Your template code here -->
+        <p>Client Info</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            clients: []
+        }
+    },
+    methods: {
+        getClients() {
+            // Fetch the clients from the API
+            fetch('http://localhost:9000/clients')
+                .then(response => response.json())
+                .then(data => {
+                    this.clients = data;
+                })
+                .catch(error => console.error(error));
+        }
+    },  
+}
+</script>
+
+<style scoped>
+/* Your style code here */
+</style>
